dec035a916fac2119445ed769d923f5e9303d28b
htao <htao@thoughtworks.com>
2018-08-24 09:50:33 +0800

    [taohui] pass param to router

--- App.js
+++ App.js
@@ -9,7 +9,10 @@ class HomeScreen extends React.Component {
         <Text>Home Screen</Text>
         <Button
           title="Go to Details"
-          onPress={() => this.props.navigation.navigate('Details')}
+          onPress={() => this.props.navigation.navigate('Details', {
+              itemId: 86,
+              otherParam: 'anything you want here',
+            })}
         />
       </View>
     );
@@ -18,12 +21,19 @@ class HomeScreen extends React.Component {
 
 class DetailsScreen extends React.Component {
   render() {
+  	const {navigation} = this.props;
+  	const itemId = navigation.getParam('itemId', 'NO-ID');
+    const otherParam = navigation.getParam('otherParam', 'some default value');
     return (
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
         <Text>Details Screen</Text>
+        <Text>itemId: {JSON.stringify(itemId)}</Text>
+        <Text>otherParam: {JSON.stringify(otherParam)}</Text>
         <Button
 		  title="Go to Details... again"
-		  onPress={() => this.props.navigation.push('Details')}
+		  onPress={() => this.props.navigation.push('Details', {
+		  	itemId: Math.floor(Math.random() * 100)
+		  })}
 		/>
 		<Button
           title="Go to Home"
